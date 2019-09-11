14521f38d4e15abd3ab37cd2aedac676e7ef3ede
Kyryl Stronko <kirstronko@gmail.com>
2019-09-11 23:35:34 +0300

    mongodb

--- src/controllers/api/index.ts
+++ src/controllers/api/index.ts
@@ -1,26 +1,8 @@
 import {RequestHandler} from 'express';
-import centersLiterals from './test-cells.json';
-
-import {Area, GridParams, CenterPoint} from '@micelord/grider';
-
-const params = GridParams.fromConfig({  
-  type: 'hex',
-  correction: 'merc',
-  cellSize: 10000,
-  isHorizontal: true,
-});
-
-const centers: CenterPoint[] = centersLiterals.map((point) => {
-  return CenterPoint.fromPlain(point, params);
-})
 
 export class ApiController {
   public handleRequest: RequestHandler = async (_req, res) => {
 
-    const area = await Area.fromCellCenters(centers);
-
-    const points = area.points.map((point) => point.toPlain());
-
-    res.send(points);
+    res.send('Hello World!');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import {MongoClient} from 'mongodb';
+
 import {Controllers, createControllers} from './controllers';
 import {createRouters, Routers} from './routers';
 import {Server} from './server';
@@ -13,6 +15,37 @@ const app: express.Application = express();
 const controllers: Controllers = createControllers();
 const router: Routers = createRouters(controllers);
 const server: Server = new Server(app, router);
+
+const client = new MongoClient(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
+
+let collection: any;
+
+client.connect()
+  .then(() => {
+    const db = client.db('micelord');
+
+    collection = db.collection('documents');
+
+    return collection.drop();
+  })
+  .then(() => {
+    return collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ]);
+  })
+  .then((result) => {    
+    console.log(result)
+    console.log("Inserted 3 documents into the collection");
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .then(() => {
+    client.close();
+  });
 
 server.listen(PORT)
   .then((): void => {
